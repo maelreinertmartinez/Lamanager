6e69a4cb40bce6e74fb0e8a90e8efdcc95aec0a3
Maël COURT <maelcourt0@gmail.com>
2024-11-05 15:18:50 +0100

    migration pour l'enseignement

--- Lamanager/database/migrations/2024_11_05_135546_create_role_table.php
+++ Lamanager/database/migrations/2024_11_05_135546_create_role_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
 
             $table->string('nom');
-            $table->integer('nombreHeure');
+            $table->integer('nombreHeure', 5);
             $table->timestamps();
         });
     }
