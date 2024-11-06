6e0cf2ab82e41482bacf77387d0e9d94b5f70770
Yannis Boisserie <yannis.boisserie@etu.unilim.fr>
2024-11-06 11:04:47 +0100

    ajout boutons partie droite première interface

---
+++ Lamanager/resources/js/Components/ChoixAnnee.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ChoixAnnee({ className, title }) {
+    return (
+      <div className={className}>
+        <h2>{title}</h2>
+      </div>
+    );
+  }
+  
+  export default ChoixAnnee;
--- Lamanager/resources/js/Components/RightPart.jsx
+++ Lamanager/resources/js/Components/RightPart.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import ChoixAnnee from "./ChoixAnnee";
+import BoutonModificationsAnnees from "./BoutonsModificationsAnnees";
+import { Trash2, Edit } from "lucide-react";
 
 function RightPart() {
   return (
     <div className="Right">
-      <h1>Right Part</h1>
+      <div className="Annees">
+        <ul className="annees-list">
+          <li><ChoixAnnee className="but-class" title="BUT 1" /></li>
+          <li><ChoixAnnee className="but-class" title="BUT 2" /></li>
+          <li><ChoixAnnee className="but-class" title="BUT 3" /></li>
+          <li><ChoixAnnee className="but-class" title="+" /></li>
+        </ul>
+      </div>
+      <div className="ModificationsAnnees">
+        <BoutonModificationsAnnees className="btn-modif-class" Icon={Trash2} />
+        <BoutonModificationsAnnees className="btn-modif-class" Icon={Edit} />
+      </div>
     </div>
   );
 }
