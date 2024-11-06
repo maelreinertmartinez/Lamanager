f6586255db63ecad3a1709109fbda9a4523994f7
Yannis Boisserie <yannis.boisserie@etu.unilim.fr>
2024-11-06 11:04:47 +0100

    ajout boutons partie droite première interface

---
+++ Lamanager/resources/js/Components/BoutonsModificationsAnnees.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BoutonModificationsAnnees({ className, Icon}) {
+    return (
+      <div className={className}>
+        <Icon width={50} height={50} color="#564787" />
+      </div>
+    );
+  }
+  
+  export default BoutonModificationsAnnees;
