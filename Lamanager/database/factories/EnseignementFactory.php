ce33a74aa62c06c2603e130eab05f24677f17f33
Druineau1 <thomas.druineau@etu.unilim.fr>
2024-11-08 15:04:38 +0100

    patch cohérence promo enseignement

--- Lamanager/database/factories/EnseignementFactory.php
+++ Lamanager/database/factories/EnseignementFactory.php
@@ -3,28 +3,33 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Promo;
 
 class EnseignementFactory extends Factory
 {
     public function definition(): array
     {
-        // Génère un promo_id aléatoire entre 1 et 18 (6 années × 3 BUT)
-        $promo_id = rand(1, 18);
-        
-        // Détermine le niveau de BUT en fonction du promo_id
-        $but_level = (($promo_id - 1) % 3) + 1; // 1, 2, ou 3
-        
-        // Détermine le chiffre R en fonction du niveau BUT
-        $r_number = match($but_level) {
-            1 => rand(1, 2),    // BUT1 -> R1 ou R2
-            2 => rand(3, 4),    // BUT2 -> R3 ou R4
-            3 => rand(5, 6),    // BUT3 -> R5 ou R6
+        // Détermine les numéros R possibles selon le nom de la promo
+        $but_level = function($promo_nom) {
+            return match($promo_nom) {
+                'BUT1' => [1, 2],    // BUT1 -> R1 ou R2
+                'BUT2' => [3, 4],    // BUT2 -> R3 ou R4
+                'BUT3' => [5, 6],    // BUT3 -> R5 ou R6
+                default => [1, 2],    // Par défaut BUT1
+            };
         };
 
         return [
-            'nom' => 'R' . $r_number . '.' . str_pad(rand(1, 15), 2, '0', STR_PAD_LEFT),
-            'promo_id' => $promo_id,
-            'alternant' => $but_level === 3, // true seulement pour BUT3
+            'nom' => function (array $attributes) use ($but_level) {
+                $promo = Promo::find($attributes['promo_id']);
+                $r_numbers = $but_level($promo->nom);
+                $r_number = $r_numbers[array_rand($r_numbers)];
+                return 'R' . $r_number . '.' . str_pad(rand(1, 15), 2, '0', STR_PAD_LEFT);
+            },
+            'alternant' => function (array $attributes) {
+                $promo = Promo::find($attributes['promo_id']);
+                return $promo->nom === 'BUT3';
+            },
             'nombre_heures_cm' => rand(10, 30),
             'nombre_heures_td' => rand(20, 40),
             'nombre_heures_tp' => rand(20, 40),
--- Lamanager/database/seeders/DatabaseSeeder.php
+++ Lamanager/database/seeders/DatabaseSeeder.php
@@ -2,23 +2,20 @@
 
 namespace Database\Seeders;
 
-use App\Models\Enseignant;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-        $this->call(EnseignantSeeder::class);
-        $this->call(SemaineSeeder::class);
-        $this->call(EnseignementSeeder::class);
-        $this->call(GroupeSeeder::class);
+        $this->call([
+            RoleSeeder::class,    
+            EnseignantSeeder::class,   
+            SemaineSeeder::class,
+            EnseignementSeeder::class,
+            GroupeSeeder::class,
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
