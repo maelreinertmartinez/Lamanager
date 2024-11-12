80bcbda53e49cfed903048619a83c295028fbdb8
Yannis Boisserie <yannis.boisserie@etu.unilim.fr>
2024-11-12 11:28:24 +0100

    ajout possibilité de défilement du tableau

--- Lamanager/resources/js/Components/Tableau.jsx
+++ Lamanager/resources/js/Components/Tableau.jsx
@@ -93,7 +93,7 @@ function EnseignementComponent({ selectedEnseignements, onRemoveEnseignement }) 
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'Total'].map((row, rowIndex) => (
+                                    {['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'Total'].map((row, rowIndex) => (
                                         <tr key={row}>
                                             <td
                                                 className="border border-black p-2"
